08dfdee2c4153cdd0a63db449c3c81526ef1da54
behindmagic9 <103569544+behindmagic9@users.noreply.github.com>
2026-06-03 14:59:33 +0530

    Enhance Broker with error handling and retry mechanism
    Refactor Broker to handle event delivery failures with retry logic and separate queues for errors and dead events.

--- broker/broker.go
+++ broker/broker.go
@@ -3,18 +3,31 @@ package broker
 import (
 	"observer/isubscriber"
 	"observer/event"
+	"fmt"
 )
 
 type Broker struct{
 	record map[string][]isubscriber.Isubscriber
 	queue []event.Event
+	errQueue []DeliveryFail
+	deadQueue []DeliveryFail
 }
 // var is not used inside the struct
+
+const MAX_RETRY int = 4
+
+type DeliveryFail struct{
+	Event event.Event
+	Subscriber isubscriber.Isubscriber
+	Retry int
+}
 
 func NewBroker() *Broker{
 	return &Broker{
 		record : make(map[string][]isubscriber.Isubscriber),
 		queue : []event.Event{},
+		errQueue : []DeliveryFail{},
+		deadQueue  : []DeliveryFail{},
 	}
 }
 
@@ -24,24 +37,56 @@ func (s *Broker) Subscribe(topic string,obs isubscriber.Isubscriber) {
 
 func (s *Broker) Notify(data event.Event) {
 	// push the events into the queue
-	append(s.queue, data)
+	s.queue = append(s.queue, data)
 }
 
 func(s *Broker) ProcessEvents(){
 	if(len(s.queue) == 0) {return} 
-	for(len(s.queue) > 0)
-		evaluate_events()
+	for(len(s.queue) > 0){
+		s.evaluateEvents()
+	}
+	for(len(s.errQueue) > 0){
+		s.evaluate_Failed_Events()
+	}
 }
 
-func evaluate_events(){
+// without adding the recieveer param will act like independent function not like memeber function of Broker
+func (s *Broker) evaluateEvents(){
 	first := s.queue[0]
 	subscriber := s.record[first.Topic]
-		for _,ob := range subscriber{
-		ob.Update(first)
+	for _,ob := range subscriber{
+		err := ob.Update(first) // as the Update gonna return the err
+		if err != nil{
+			failure := DeliveryFail{
+				Event : first,
+				Subscriber : ob,
+				Retry : 1,
+			}
+			s.errQueue = append(s.errQueue, failure)
+		}
 	}
-	s.queue := s.queue[1:]
+	s.queue = s.queue[1:]
 }
 
+// without adding the recieveer param will act like independent function not like memeber function of Broker
+func (s *Broker) evaluate_Failed_Events(){
+	first := s.errQueue[0]
+
+	err := first.Subscriber.Update(first.Event)
+	if err != nil 
+	{
+		first.Retry++
+		if first.Retry < MAX_RETRY{
+			// log it cant process them and so dropping
+			s.errQueue = append(s.errQueue, first)
+		}else if  first.Retry >= MAX_RETRY {
+			s.deadQueue = append(s.deadQueue, first)
+			// can later implement logging here by loggin the complete imformation from the Delivery Struct of this
+			fmt.Println("droping event as cant be able to process it after multiple retries")
+		}
+	}
+	s.errQueue = s.errQueue[1:]
+}
 
 func (s *Broker) Unsubscribe(topic string, obs isubscriber.Isubscriber){
 	subscribers := s.record[topic]
